fb0d34617f374a64fee622c083cc32ee7d596f72
Alan Hardman <alanaktion@gmail.com>
2019-09-18 14:18:09 -0600

    Finishing basic Tailwind rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,66 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
+    colors: {
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+    },
+    fontWeight: {
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {}
   },
-  variants: {},
-  plugins: []
+  variants: {
+    appearance: [],
+    backgroundAttachment: [],
+    backgroundColor: ['hover', 'focus'],
+    backgroundPosition: [],
+    backgroundRepeat: [],
+    backgroundSize: [],
+    borderCollapse: [],
+    borderColor: ['hover', 'focus'],
+    borderRadius: [],
+    borderStyle: [],
+    borderWidth: [],
+    boxShadow: ['hover', 'focus'],
+    cursor: [],
+    fill: [],
+    fontFamily: [],
+    fontSize: [],
+    fontSmoothing: [],
+    fontStyle: [],
+    fontWeight: [],
+    opacity: ['hover', 'focus'],
+    outline: ['focus'],
+    placeholderColor: [],
+    pointerEvents: [],
+    stroke: [],
+    textColor: ['hover', 'focus'],
+    textDecoration: ['hover', 'focus'],
+    textTransform: [],
+    userSelect: [],
+    whitespace: [],
+  },
+  plugins: [],
+  corePlugins: {
+    backgroundAttachment: false,
+    float: false,
+    placeholderColor: false,
+  }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 const mix = require('laravel-mix');
 
 mix.postCss('_css/theme.css', 'public/css/', [
-        require('postcss-easy-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
-            content: ['./**/*.html'],
-        }),
-    ]);
+    require('postcss-easy-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./**/*.html'],
+    }),
+  ]);
