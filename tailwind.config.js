eb155dc4568f0e0eb61e4e1a6247d58f98e14fa7
Alan Hardman <alanaktion@gmail.com>
2020-03-03 17:12:40 -0700

    Adjusting type

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,16 @@ module.exports = {
     fontFamily: {
       mono: ['JetBrains Mono', 'Cascadia Code', 'Fira Code', ...fontFamily.mono],
     },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.3rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+    },
     fontWeight: {
       light: '300',
       normal: '400',
